7e0350dea866d5ed3bfc1f22cb2bc1fe68efa05b
Ampherost <Ampherost235@gmail.com>
2025-07-15 21:13:58 -0700

    leveraged unity new input system to handle movement
    - created a new input action
    - generated a C# script from that input action
    - used it to make a Player Movement Script (modularity)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("Movement speed in units/sec")]
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private PlayerInputActions inputActions;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
+
+        // Subscribe to the Move action
+        inputActions.Player.Move.performed += OnMovePerformed;
+        inputActions.Player.Move.canceled += OnMoveCanceled;
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext ctx)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        // Apply velocity; Rigidbody2D.gravityScale should be 0
+        // rb.velocity = moveInput * moveSpeed;
+        // Turns out that the line above is outdated
+
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
+
